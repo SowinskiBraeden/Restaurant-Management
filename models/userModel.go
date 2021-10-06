2de5b87bb9de248d30b497f13decd54759014a6c
Braeden57 <sowinskibraeden@gmail.com>
2021-10-05 19:28:41 -0700

    Update controllers

---
+++ models/userModel.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitave"
+)
+
+type User struct {
+	ID            primitave.ObjectID `bson:"_id"`
+	First_name    *string            `json:"first_name" validate:"required,min=2,max=100"`
+	Last_name     *string            `json:"last_name" validate:"required,min=2,max=100"`
+	Password      *string            `json:"Password" validate:"required,min=8`
+	Email         *string            `json:"email" validate:"email,required"`
+	Avatar        *string            `json:"avatar"`
+	Phone         *string            `json:"phone" validate:"required"`
+	Token         *string            `json:"token"`
+	Refresh_token *string            `json:"refresh_token"`
+	Created_at    time.Time          `json:"created_at"`
+	Updated_at    time.Time          `json:"updated_at"`
+	User_id       string             `json:"user_id"`
+}
